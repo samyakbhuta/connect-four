09900b77fed2b1e26154f937b048b1886f2dbdd6
Samyak Bhuta <samyak.bhuta@gmail.com>
2014-05-20 19:53:37 +0530

    Adds user prompt, delays game after win

--- js/app.js
+++ js/app.js
@@ -4,11 +4,10 @@ $(document).ready(function(){
 	// Init, creates a new game.
 	// Ask for players' names.
 	// Displays the board.
-	// var p1 = window.prompt("First player's name", "P1");
-	// var p2 = window.prompt("Second player's name", "P2");
 
-	var p1 = 'P1';
-	var p2 = 'P2';
+	var p1 = window.prompt("First player's name", "P1");
+	var p2 = window.prompt("Second player's name", "P2");
+
 	var game = new ConnectFour (p1,p2);
 	$('#message-pane').html( game.currentPlayer() +
 				', your turn. Click/tap on column where you want to drop your disc.');
@@ -28,7 +27,12 @@ $(document).ready(function(){
 			// Game over. Show message with result and starts new game.
 			$('#message-pane').html('Game Over. Winner is ' + status + ' !' +
 				' Click on any column to start a new game.');
-			game = new ConnectFour (p1,p2);
+
+			// Halts the view for user to see the end status just after win.
+			setTimeout(function(){
+				game = new ConnectFour (p1,p2);
+			},500);
+
 		} else {
 			// Continue, display the message wrt to whose turn it is
 			$('#message-pane').html( game.currentPlayer() +
